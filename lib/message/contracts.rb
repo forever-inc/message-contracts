02a58e6a95d01b8e8e3f13ac7452558603eb98ae
alex simeone <asimeone@forever.com>
2021-11-29 15:22:40 -0500

    initial commit

---
+++ lib/message/contracts.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "contracts/version"
+
+module Message
+  module Contracts
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/message/contracts_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Message::Contracts do
+  it "has a version number" do
+    expect(Message::Contracts::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
