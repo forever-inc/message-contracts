60d9adc5988da4d2dba4cd4ac7a525553a6d3c30
alex simeone <asimeone@forever.com>
2026-06-17 09:52:14 -0400

    adding conversion topics

--- lib/message-contracts/topics/routing_keys.rb
+++ lib/message-contracts/topics/routing_keys.rb
@@ -4,7 +4,8 @@ class RoutingKeys
   USER_CHANGED = "user.changed"
   USER_CREATED = "user.created"
   USER_SYNC = "user.sync"
-  BOX_CHANGED = "box.changed"
-  BOX_ORIGINATED = "box.originated"
   NOTIF_USER_CHANGED = "notif.user.changed"
+  BOX_RECEIVED = "box.received"
+  BOX_AWAITING_PAYMENT = 'box.awaiting_payment'
+  BOX_RETURNED = 'box.returned'
 end
